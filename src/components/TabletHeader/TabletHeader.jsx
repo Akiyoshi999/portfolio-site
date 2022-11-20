c0bcbe5f3857c46ef81e73eeed4fb8e8f19ba0b9
Akiyoshi <zundokokiyoshi999@gmail.com>
2022-11-20 10:56:30 +0900

    アイコンにリンク設定

--- src/components/TabletHeader/TabletHeader.jsx
+++ src/components/TabletHeader/TabletHeader.jsx
@@ -1,5 +1,5 @@
 import Toolbar from "@mui/material/Toolbar";
-import { AppBar, Box, Stack, Typography } from "@mui/material";
+import { AppBar, Box, Link, Stack, Typography } from "@mui/material";
 import { TabletNavMenu } from "./TabletNavMenu";
 import navBarItems from "../data/const/navBarItems";
 
@@ -12,12 +12,13 @@ export default function TabletHeader() {
       sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}
     >
       <Toolbar sx={{ maxWidth: "xl", height: "64px" }}>
-        <Box
-          component="img"
-          src="/images/logo.png"
-          sx={{ height: "100%", width: "auto", mt: 1 }}
-          // sx={{ height: "62px", width: "62px", mt: 1 }}
-        />
+        <Link href="/" position="relative" sx={{ height: "100%" }}>
+          <Box
+            component="img"
+            src="/images/logo.png"
+            sx={{ height: "100%", width: "auto", pt: 1 }}
+          />
+        </Link>
         <Typography
           variant="h5"
           sx={{
