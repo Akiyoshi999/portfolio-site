930895ab8ce5200b48dd13d27ba662ee33a27d03
Akiyoshi <zundokokiyoshi999@gmail.com>
2023-04-23 10:49:54 +0900

    ファイル名修正

---
+++ src/const/contactItem.js
@@ -0,0 +1,20 @@
+const contactSection = {
+  title: "Contact",
+  body: `
+  簡単ではありますが、お問い合わせのフォームをご用意しました。
+          ホームページの改善点や私に連絡がしたい方は、こちらからご連絡ください。
+  `,
+  elId: "contact-section",
+};
+const alerts = {
+  success: {
+    severity: "success",
+    msg: "メッセージありがとうございます。後ほどご連絡いたします。",
+  },
+  error: {
+    severity: "error",
+    msg: "エラーが発生しました。恐れいりますが、時間を空けてもう一度お問い合わせください。",
+  },
+};
+
+export { contactSection, alerts };
