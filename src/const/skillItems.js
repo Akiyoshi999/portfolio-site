c6e18297fa7e05fb26dd66cc3fb47f1f6fcf0f65
Akiyoshi <zundokokiyoshi999@gmail.com>
2023-04-30 11:48:52 +0900

    スキルセットのコンテンツ修正 #27

--- src/const/skillItems.js
+++ src/const/skillItems.js
@@ -1,13 +1,18 @@
-import { Rating as MuiRating } from "@mui/material";
+import { Rating as MuiRating, Typography } from "@mui/material";
 import CardMembershipIcon from "@mui/icons-material/CardMembership";
 import HomeRepairServiceIcon from "@mui/icons-material/HomeRepairService";
+import HandymanIcon from "@mui/icons-material/Handyman";
 import RoomPreferencesIcon from "@mui/icons-material/RoomPreferences";
 import MonitorIcon from "@mui/icons-material/Monitor";
+import StorageIcon from "@mui/icons-material/Storage";
 const Rating = (props) => <MuiRating readOnly {...props}></MuiRating>;
 
 const skillSection = {
   title: "My Skill Set",
-  body: "修練のすえ身につけたスキルをグラフィカルにまとめました。広く浅くではありますがフロントからバックエンドまで幅広くスキルアップに取り組んできました。オールラウンドに対応できる点が強みですが、全体的に理解が浅く、専門性の低さを認識しています。今後はバックエンド系の開発（クラウド含む）の業務に携わる比率を増やし、少しずつ専門性を高めていきたいと考えています。",
+  body: `私がこれまでに習得してきたスキルをテーブル形式にまとめました。
+  深く理解しているわけではありませんが、フロントエンド、バックエンド、インフラ、OSなどの知識を幅広く持っています。
+  多様な角度から物事を見ることが私の強みです。
+  色々な知識に触れていくうちにインフラや開発の分野に強く興味を惹かれているので、今後はその分野の仕事に携わりたいと考えています。`,
   elId: "skill-skills-section",
 };
 
@@ -27,7 +32,8 @@ const commonHead = ["技術", "スキル"];
 const frontSkills = {
   title: "フロントエンド",
   titleIcon: <MonitorIcon />,
-  subheader: "aaaaaaaaaaaaaaaa",
+  subheader: `HPはJS/Reactで開発したので、基本的なところは習得できていると思います。
+  バックエンドはLaravel、フロントエンドはVue.jsの環境なども勉強のため構築したことがあります。`,
   header: commonHead,
   titleIcon: <MonitorIcon />,
   skills: [
@@ -35,41 +41,52 @@ const frontSkills = {
     ["HTML/CSS", <Rating value={2} />],
     ["React.js", <Rating value={2} />],
     ["Vue.js", <Rating value={2} />],
-    ["CSS FW", <Rating value={1} />],
   ],
 };
 
 const backendSkills = {
   title: "バックエンド",
   titleIcon: <RoomPreferencesIcon />,
-  subheader: "aaaaaaaaaaaaaaaa",
+  subheader: `Laravelで掲示板やREST API など実装したことがあるので、phpは一通り触れます。
+  Lambdaなどは基本的にPythonで実装しています。`,
   header: commonHead,
   skills: [
     ["PHP/Laravel", <Rating value={3} />],
     ["Python", <Rating value={2} />],
     ["RDBS", <Rating value={2} />],
-    ["Nginx", <Rating value={2} />],
+    ["NoSQL", <Rating value={1} />],
   ],
 };
 
 const devSkills = {
   title: "開発",
-  titleIcon: <HomeRepairServiceIcon />,
-  subheader: "aaaaaaaaaaaaaaaa",
+  titleIcon: <HandymanIcon />,
+  subheader: `開発に必要なツールは一通り触っています。
+  プログラムを書いた場合は、最低限のテストは書くように意識してます。
+  CI/CD環境もAWSとGithubとCircle CIで構築したことがあります。`,
   header: commonHead,
   skills: [
-    ["Linux", <Rating value={3} />],
-    ["Github", <Rating value={3} />],
+    ["Git/Github", <Rating value={3} />],
     ["Docker/Docker Compose", <Rating value={2} />],
-    ["AWS", <Rating value={2} />],
     ["CircleCI", <Rating value={1} />],
   ],
 };
 
+const infraSkills = {
+  title: "インフラ・サーバー",
+  titleIcon: <StorageIcon />,
+  subheader: `AWSはこのHPの作成や、資格を勉強した際に多くのサービスを使用しているので、使えます。`,
+  header: commonHead,
+  skills: [
+    ["AWS", <Rating value={3} />],
+    ["Linux", <Rating value={3} />],
+    ["Apache/NodeJS", <Rating value={1} />],
+  ],
+};
 const certification = {
   title: "資格",
-  subheader: "aaaaaaaaaaaaaaaa",
   titleIcon: <CardMembershipIcon />,
+  subheader: `現在は"AWS Certified Security - Specialty"を学習中です。`,
   skills: [
     ["Linuc レベル 1"],
     ["AWS Certified Solutions Architect Associate"],
@@ -80,9 +97,9 @@ const certification = {
 
 const skillList = [
   frontSkills,
-  frontSkills,
   backendSkills,
   devSkills,
+  infraSkills,
   certification,
 ];
 
